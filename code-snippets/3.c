9c8887735de2a6371a109fbd21a4b9865f0d4317
Tegveer Singh <tegveer2211@gmail.com>
2021-09-12 01:46:18 -0400

    Added code snippets 2

---
+++ code-snippets/3.c
@@ -0,0 +1,24 @@
+// Program for Linux Security vulnerability CVE-2021-38160
+// data corruption or loss can be triggered by an untrusted device that supplies a buf->len value exceeding the buffer size.
+#include <stdio.h>
+#include <string.h>
+
+struct buffer
+{
+    char *buf;   // Message string
+    size_t size; // Size of string
+    size_t len;  // Length occupied
+};
+
+int main()
+{
+    char buf0buf[] = "This is a message";
+    struct buffer buf0 = {buf0buf, 0, 0};
+    struct buffer *buf;
+    buf = &buf0;
+    size_t length = 20;
+    buf->size = strlen(buf->buf);
+    // Fix for Buffer Overflow
+    buf->len = buf->size > length ? length : buf->size;
+    return 0;
+}
---
+++ code-snippets/5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+struct rtas_args
+{
+    int nargs;
+    int args[16];
+};
+
+int main()
+{
+    int arr[16] = {1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    struct rtas_args old = {17, arr};
+    struct rtas_args *args;
+    args = &old;
+    if (args->nargs >= sizeof(args->args))
+    {
+        return EINVAL;
+    }
+    else
+    {
+        return 0;
+    }
+}
